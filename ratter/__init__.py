63a94f6501bad97a53757e19854b127ef916b3cb
Fabian Meyer <fabian.meyer@mailbox.org>
2019-05-13 23:44:39 +0200

    extended __init__

--- ratter/__init__.py
+++ ratter/__init__.py
@@ -1,1 +1,6 @@
+__name__ = "ratter"
 __version__ = '0.1.0 dev'
+__author__ = "Fabian Meyer"
+__maintainer__ = "Fabian Meyer"
+__email__ = "fabian.meyer@mailbox.org"
+__url__ = "https://gitlab.cc-asp.fraunhofer.de/fmeyer/ratter"
