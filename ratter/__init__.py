800a1146bd71dec5a0a38821c92fa4461b90bcee
Fabian Meyer <fabian.meyer@mailbox.org>
2019-05-29 00:12:02 +0200

    added summary description

--- ratter/__init__.py
+++ ratter/__init__.py
@@ -4,3 +4,5 @@ __author__ = "Fabian Meyer"
 __maintainer__ = "Fabian Meyer"
 __email__ = "fabian.meyer@mailbox.org"
 __url__ = "https://gitlab.cc-asp.fraunhofer.de/fmeyer/ratter"
+__summary__ = """calculate coherent reflection, absorption
+and transmission of thin layer stacks"""
