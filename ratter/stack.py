62087692484080a4c520dac4f4709bc8a9144bdc
Fabian Meyer <fabian.meyer@mailbox.org>
2019-06-12 21:04:22 +0200

    added docstrings

--- ratter/stack.py
+++ ratter/stack.py
@@ -81,9 +81,18 @@ class Layerstack(object):
 class Layer(object):
     def __init__(self, name, material, thickness_value=None):
         """
-        name
-        material
-        thickness can be a formula
+        A layer is specified by a material and a thickness. You can give
+        the layer a name, for example "bulk" or "anti-reflection coating".
+
+        Parameters
+        ----------
+        name : string
+            descriptive name for the layer
+        material : Material
+            Material that the layer is made of
+        thickness_value : float (optional)
+            if the thickness of the layer is fixed, and not to be
+            used as a variable, you can give the thickness here
         """
         self.name = name
         self.material = material
@@ -92,6 +101,14 @@ class Layer(object):
 
     @property
     def substitutions(self):
+        """List of symbol-value pairs, that are known substitutions
+        to simplify the description of this layer
+
+        Returns
+        -------
+        list of tuples(sympy.Symbol, value)
+            a list of symbols with the values they can be replaced with
+        """
         if self.thickness_value is None:
             res = []
         else:
