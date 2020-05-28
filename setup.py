a3534a90006df8650d1f5bd3734fd581f6a97cad
Fabian Meyer <fabian.meyer@mailbox.org>
2020-05-28 23:24:29 +0200

    avoid invocation of __init__ in setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 from setuptools import setup
-import ratter
+from os.path import join
 
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
+with open(join('ratter', 'version.py')) as version_file:
+    lines = version_file.readlines()
+    metadict = dict()
+
+    for line in lines:
+        try:
+            k, v = line.split('=')
+            metadict.update({k.strip(): v.strip().strip('"')})
+        except ValueError:
+            pass
+
 setup(
-    name=ratter.__name__,
-    version=ratter.__version__,
-    author=ratter.__author__,
-    author_email=ratter.__email__,
-    description=ratter.__summary__,
-    url=ratter.__url__,
+    name=metadict['__name__'],
+    version=metadict['__version__'],
+    author=metadict['__author__'],
+    author_email=metadict['__email__'],
+    description=metadict['__summary__'],
+    url=metadict['__url__'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
